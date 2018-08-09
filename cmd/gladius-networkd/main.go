1a4287414388c201da6002c4fa175f5aaf4f3e9a
Alex Godwin <alexkgodwin@gmail.com>
2018-08-09 16:36:41 -0400

    Moved back to service manager

--- cmd/gladius-networkd/main.go
+++ cmd/gladius-networkd/main.go
@@ -2,20 +2,9 @@ package main
 
 import (
 	"github.com/gladiusio/gladius-networkd/networkd"
-	"github.com/gladiusio/gladius-utils/config"
-	"github.com/gladiusio/gladius-utils/init/manager"
 )
 
 // Main - entry-point for the service
 func main() {
-	// Setup config handling
-	config.SetupConfig("gladius-networkd", config.NetworkDaemonDefaults())
-	// Define some variables
-	name, displayName, description :=
-		"GladiusNetworkDaemon",
-		"Gladius Network (Edge) Daemon",
-		"Gladius Network (Edge) Daemon"
-
-	// Run the function "run" in newtworkd as a service
-	manager.RunService(name, displayName, description, networkd.Run)
+	networkd.SetupAndRun()
 }
